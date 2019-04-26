fc55e7008d45fe4c5b4d987f66839fd469a5f7a7
Joslyn-Jiang <2413136630@qq.com>
2019-04-26 08:52:13 +0800

    new

--- src/views/new/new.vue
+++ src/views/new/new.vue
@@ -2,6 +2,13 @@
 <div>
     <div id="main">哈哈哈哈</div>
     <div id="slidebar">啦啦啦啦</div>
+     <ul>
+         <li></li>
+         <li></li>
+         <li></li>
+         <li></li>
+     </ul>
+<div class="btn-dribble">哈哈哈</div>
     </div>    
 </template>
 <script>
@@ -22,5 +29,40 @@ export default {
     width: $width;
     height: $height;
     border: $border; 
+}
+$list-bg:red,orange,blue,skyblue;
+ul{
+    list-style: none;
+    >li{
+        height: 30px;
+        @each $c in $list-bg{
+            $i:index($list-bg,$c);
+            &:nth-child(#{$i+1}){
+                background: nth($list-bg,$i);
+            }
+        }
+    }
+}
+// 默认主题
+$default-theme : (
+  base-color: #ddd,
+  border-color: #000
+);
+//红色主题
+$red-theme : (
+  base-color: red,
+  border-color: red
+);
+//定义映射集合
+$themes: (
+  default-theme: $default-theme,
+  red-theme: $red-theme
+);
+@mixin base-background(){
+    @each $themename , $theme in $themes {
+        [data-theme = '#{$themename}'] & {
+            background: map-get($map: $theme, $key: base-color )
+        }
+    }
 }
 </style>
